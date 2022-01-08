6bfad20cc4a34371945cf9950e59243b10f01fb5
Szymon Konopek <sk.gornik@gmail.com>
2022-01-08 17:40:21 +0100

    comments

--- scripts/sortData.js
+++ scripts/sortData.js
@@ -1,36 +1,37 @@
-function sort(language_array){
-    let unique_languages = []
-    let lang_count = []
-
-    language_array.map(item1 =>{
-        if (!unique_languages.includes(item1)){
-            unique_languages.push(item1)
+function sort(array){
+    let unique_items = []
+    let item_count = []
+    
+    //tworzenie tablicy unique_items z unikalnymi "itemami" oraz liczbe ich wystąpień  w tablicy item_count
+    array.map(item1 =>{
+        if (!unique_items.includes(item1)){
+            unique_items.push(item1)
 
             count = 0
-            language_array.map(item2 => {
+            array.map(item2 => {
                 
                 if (item1 == item2){
                     count += 1
                 }
             })
-            lang_count.push(count)
+            item_count.push(count)
         }
 
     })
 
     var next = true;
-
+    //sortowanie bąbelkowe
     while(next){
         var skip = true;
-        for (var i = 0; i < lang_count.length; i++){
-            if (lang_count[i] < lang_count[i+1]){
-                temp = lang_count[i]
-                lang_count[i] = lang_count[i+1]
-                lang_count[i+1] = temp
+        for (var i = 0; i < item_count.length; i++){
+            if (item_count[i] < item_count[i+1]){
+                temp = item_count[i]
+                item_count[i] = item_count[i+1]
+                item_count[i+1] = temp
 
-                temp = unique_languages[i]
-                unique_languages[i] = unique_languages[i+1]
-                unique_languages[i+1] = temp
+                temp = unique_items[i]
+                unique_items[i] = unique_items[i+1]
+                unique_items[i+1] = temp
                 skip = false;
             }
         }
@@ -40,7 +41,7 @@ function sort(language_array){
         }
 
     }
-    return {"items": unique_languages,"count":lang_count}
+    return {"items": unique_items,"count":item_count}
 }
 
 function sortList(array){
